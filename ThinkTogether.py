f16ceb00db8f467d071c067a5ffd04e4093b21c5
bhushanwalke <bhushan.h2@gmail.com>
2015-10-03 16:02:07 -0700

    Login module with Structure

--- ThinkTogether.py
+++ ThinkTogether.py
@@ -1,12 +1,17 @@
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
-app = Flask(__name__)
 
+from ThinkTogether import app
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+app.run(debug=True)
