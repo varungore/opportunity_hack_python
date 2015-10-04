5a9662275cbec65dd62be4aa96948c22c8c0ef9a
Varun Gore <vgoredev@gmail.com>
2015-10-03 21:18:26 -0700

    refactored

--- ThinkTogether/__init__.py
+++ ThinkTogether/__init__.py
@@ -22,7 +22,7 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
-from ThinkTogether.controllers.users import mod_auth as auth_module
+from ThinkTogether.controllers.user import mod_auth as auth_module
 
 # Register blueprint(s)
 app.register_blueprint(auth_module)
